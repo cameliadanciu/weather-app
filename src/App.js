0c79baaaacf0000641ee1ba48d6b7ec5cba7f2b8
cameliadanciu <74072816+cameliadanciu@users.noreply.github.com>
2022-02-27 16:11:30 +0000

    temperature and weather added
    temperature box and weather box created
    +styling added

--- src/App.js
+++ src/App.js
@@ -24,7 +24,16 @@ function App() {
         <div className="location-box">
           <div className='location'>New York City</div>
           <div className='date'>{dateBuilder(new Date())}</div>
-        </div>          
+        </div>
+
+        <div className='weather-box'>
+          <div className='temp'>
+            15ºC
+          </div>
+          <div className='weather'>
+            Sunny
+          </div>
+        </div>  
       </main>
     </div>
   );
